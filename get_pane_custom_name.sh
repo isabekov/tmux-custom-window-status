1cf53d919748a8debd925ef1c2947a97501bd816
Altynbek Isabekov <aisabekov@ku.edu.tr>
2022-05-30 15:47:16 +0200

    get_pane_custom_name.sh: Abbreviate first  characters argument is added.

--- get_pane_custom_name.sh
+++ get_pane_custom_name.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
-# Arg1: #{pane_pid}
-# Arg2: #{pane_current_command}
-# Arg3: #{pane_current_path}
+# arg1: #{pane_pid}
+# arg2: #{pane_current_command}
+# arg3: #{pane_current_path}
+# arg4: number of characters after which directory name shortening will be applied
+# arg5: number of characters to show from the beginning ("front") of the directory name (condition: arg5 < arg4)
+
+if [ $# -eq 4 ]; then
+   na=${4}
+   nf=$(( (${na} - 3) /2 ))
+   nb=$(( ${nf} + $(( (${na} - 3)%2)) ))
+fi
+
+if [ $# -eq 5 ]; then
+  na=${4}
+  if [ ${5} -le $(( ${na}-3 )) ]; then
+     nf=${5}
+     nb=$(( ${na} - 3 - ${nf} ))
+  else
+     nf=$(( ${na} - 3 ))
+     nb=0
+  fi
+fi
+
 
 curps=`ps -f -o cmd --no-headers --ppid $1`
-#echo -n $2
+
 if [ -z ${curps} ]; then
   echo -n "$2"
 else
@@ -20,11 +40,16 @@ else
   fi
 fi
 
+echo -n "#[fg=color21]"
 # Check if command is is "sh", "-sh", "bash", "-bash", "zsh", "-zsh"
 if [[ "$2" =~ ^[-]{0,1}(ba|z|)sh ]]; then
   d="${3/\/home\/`whoami`/\~}"
-  if [ ${#d} -gt 20 ]; then
-      echo -n " ${d::8}...${d: -9}"
+  if [ ${#d} -gt ${na} ]; then
+      if [ ${nb} -ne 0 ]; then
+          echo -n " ${d::${nf}}...${d: -${nb}}"
+      else
+          echo -n " ${d::${nf}}..."
+      fi
   else
       echo -n " ${d}"
       [ "${d}" = "~" ] && echo -n "/"
