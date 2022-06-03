01d067bd47d3015ac64bdc95f6d0cd4d8e274c28
Altynbek Isabekov <aisabekov@ku.edu.tr>
2022-06-03 21:48:55 +0200

    First draft version of the plugin.

--- custom-status.tmux
+++ custom-status.tmux
@@ -1,14 +1,47 @@
 
 #!/usr/bin/env bash
-set -x
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-main(){
-    tmux set-window-option -g "window-status-format" "#[fg=red,bold]#I#[fg=white]:#[default] `\
-              `#(${CURRENT_DIR}/get_pane_custom_name.sh `\
-              `\"#{pane_pid}\" \"#{pane_current_command}\" \"#{pane_current_path}\" 20 7)"
+custom_pane_name="#(${CURRENT_DIR}/get_pane_custom_name.sh `\
+                  `\"#{pane_pid}\" \"#{pane_current_command}\" \"#{pane_current_path}\" 20 7)"
+
+placeholder="\#{custom_pane_name}"
+
+get_tmux_window_option() {
+	local option=$1
+	local default_value=$2
+	local option_value=$(tmux show-option -gqvw "$option")
+	if [ -z "$option_value" ]; then
+		echo "$default_value"
+	else
+		echo "$option_value"
+	fi
+}
+
+set_tmux_window_option() {
+	local option="$1"
+	local value="$2"
+	tmux set-option -gqw "$option" "$value"
+}
+
+do_interpolation() {
+	local string="$1"
+	local interpolated="${string/${placeholder}/${custom_pane_name}}"
+	echo "$interpolated"
+}
+
+update_tmux_window_option() {
+	local option="$1"
+	local option_value="$(get_tmux_window_option "$option")"
+	local new_option_value="$(do_interpolation "$option_value")"
+	set_tmux_window_option "$option" "$new_option_value"
+}
+
+main() {
+  update_tmux_window_option "window-status-format"
+  update_tmux_window_option "window-status-current-format"
 }
 
 main
--- get_pane_custom_name.sh
+++ get_pane_custom_name.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
+# This scripts produces (prints) a custom name for a pane in a tmux session.
+#
+# Features:
+# - instead of the short #{pane_current_command} with no arguments, full name
+#   of the executed command (with arguments) of the current process is abbreviated
+# - directory names are printed only for "sh", "zsh" and "bash" shells
+# - abbreviation of directory name from the beginning and the end of its name
+# - "/home/`whoami`"" is replaced with "~/" in directory name
+# - SSH session names (as invoked) are highlighted with a different color,
+#   no directory name is displayed
+
 # arg1: #{pane_pid}
 # arg2: #{pane_current_command}
 # arg3: #{pane_current_path}
-# arg4: number of characters after which directory name shortening will be applied
-# arg5: number of characters to show from the beginning ("front") of the directory name (condition: arg5 < arg4)
+# arg4: number of characters used for abbreviation of directory names
+# arg5: number of characters to abbreviate from the beginning of directory name (condition: arg5 < arg4)
 
 if [ $# -eq 4 ]; then
+   # Length of the abbreviated directory name
    na=${4}
+   # Subtract length of placeholder for "..." from the final length,
+   # and try to assign equal lengths for abbreviation from front ("nf") and back ("nb")
    nf=$(( (${na} - 3) /2 ))
    nb=$(( ${nf} + $(( (${na} - 3)%2)) ))
 fi
@@ -14,6 +28,7 @@ fi
 if [ $# -eq 5 ]; then
   na=${4}
   if [ ${5} -le $(( ${na}-3 )) ]; then
+     # Directory name should be abbreviated to "nf" characters from the beginning
      nf=${5}
      nb=$(( ${na} - 3 - ${nf} ))
   else
@@ -22,36 +37,52 @@ if [ $# -eq 5 ]; then
   fi
 fi
 
-
+# Retrieve full executed command name (with arguments) of the current process in the pane
 curps=`ps -f -o cmd --no-headers --ppid $1`
 
 if [ -z ${curps} ]; then
   echo -n "$2"
 else
+  # Delete all characters till the last occurence (counted from back)
+  # of a whitespace inclusively from back of string "$a"
   a=${curps%% *}
+
+  # Delete all characters till the last occurence (counted from front)
+  # of a whitespace inclusively from front of string "$a"
   b=${curps##* }
+
   if [ $a = $b ]; then
+     # There are no arguments to the command "$a"
      echo -n "$a"
-  else \
+  else
      if [ ${a::3} = 'ssh' ]; then
-         echo -n "#[fg=color20,bold]"
+         # Highlight SSH sessions with a special color
+         echo -n "#[fg=red,bold]"
      fi
      echo -n "${a##*/} ${b##*/}"
   fi
 fi
 
-echo -n "#[fg=color21]"
-# Check if command is is "sh", "-sh", "bash", "-bash", "zsh", "-zsh"
+echo -n "#[fg=color90]"
+# Check if executed command is is "sh", "-sh", "bash", "-bash", "zsh", "-zsh"
+# Dash ("-") in front of a shell name means it is a login shell
 if [[ "$2" =~ ^[-]{0,1}(ba|z|)sh ]]; then
+
+  # If it is user's home directory, replace it with "~"
   d="${3/\/home\/`whoami`/\~}"
+
   if [ ${#d} -gt ${na} ]; then
       if [ ${nb} -ne 0 ]; then
+          # Abbreviate directory name from the beginning and the end
           echo -n " ${d::${nf}}...${d: -${nb}}"
       else
+          # Abbreviate directory name only from the beginning of the name
           echo -n " ${d::${nf}}..."
       fi
   else
+      # Directory name is shorter than allowed length, display it fully
       echo -n " ${d}"
+      # If it is user's home directory "~", append "/" at the end
       [ "${d}" = "~" ] && echo -n "/"
   fi
 fi
