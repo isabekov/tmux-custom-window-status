f686ee75dd7a35fe697d074772035c0be151194a
Altynbek Isabekov <aisabekov@id5.io>
2023-01-02 17:20:21 +0100

    Replace home directory name with "~/" for MacOS as well.

--- get_pane_custom_name.sh
+++ get_pane_custom_name.sh
@@ -72,7 +72,8 @@ echo -n "#[fg=color90]"
 if [[ "$2" =~ ^[-]{0,1}(ba|z|.{0})sh ]]; then
 
   # If it is user's home directory, replace it with "~"
-  d="${3/\/home\/`whoami`/'~'}"
+  [[ "$OSTYPE" == "linux-gnu" ]] && d="${3/\/home\/`whoami`/'~'}"
+  [[ "$OSTYPE" =~ "darwin" ]] && d="${3/\/Users\/`whoami`/'~'}"
 
   if [ ${#d} -gt ${na} ]; then
       if [ ${nb} -ne 0 ]; then
